ef401924a7dd06b476a77f1538c7d6427258aa98
Joel Eaves <joel.eaves@colorado.edu>
2025-02-24 12:11:51 -0700

    Fixed Spaces in Module IV and Uploaded an "Optimized" Force Loop with Cell Neighbor Lists

--- Molecular Dynamics/src/ForceFields.jl
+++ Molecular Dynamics/src/ForceFields.jl
@@ -1,9 +1,10 @@
 module ForceFields
 
+using ..CellLists
+export ForceField, compute_pair_interaction, compute_forces!, update_energy!
+
 abstract type ForceField end
 
-export ForceField, compute_pair_interaction, compute_forces!, update_energy!
-# Generic force calculation loop that works for any force field
 function compute_forces!(ff::ForceField)
     r = ff.base.atoms.r
     f = ff.base.atoms.f
@@ -12,31 +13,49 @@ function compute_forces!(ff::ForceField)
     
     V = W = 0.0
     f .= 0.0
-    @inline dot(x,y) = sum(x .* y)
     rij = zeros(3)
-    ri = zeros(3)
     rcut² = isnothing(ff.parameters.rcut) ? Inf : ff.parameters.rcut^2
-    for i ∈ 1:N
-        ri = r[:,i]
-        for j ∈ (i+1):N
-            rij .= ri - r[:, j]
-            rij .= rij .- L .* round.(Int64, rij ./ L)
-            r2 = dot(rij, rij)
-            if r2 < rcut²
-                fmag, vij = compute_pair_interaction(ff, r2)  # This is what changes per force field
-                fij = fmag*rij
-                f[:, i] .+= fij
-                f[:, j] .-= fij
-                V += vij
-                W += dot(fij, rij)
+    if isa(ff.cells, CellList)
+        update_cells!(ff.cells, r, L)
+        for i in 1:N
+            ri = r[:,i]
+            for j in get_cell_neighbors(ff.cells, i, r, L, rcut²)
+                rij .= ri - r[:,j]
+                rij .= rij .- L .* round.(Int64, rij ./ L)
+                r2 = sum(rij .* rij)
+                if r2 < rcut²
+                    fij, vij = compute_pair_interaction(ff, r2)
+                    f[:,i] .+= fij.*rij
+                    f[:,j] .-= fij.*rij
+                    V += vij
+                    W₀ = sum(fij.*rij)
+                    W += i < j ? W₀ : -W₀
+                end
+            end
+        end
+    else
+        for i in 1:(N-1)
+            ri = r[:,i]
+            for j in (i+1):N
+                rij .= ri - r[:,j]
+                rij .= rij .- L .* round.(Int64, rij ./ L)
+                r2 = sum(rij .* rij)
+                if r2 < rcut²
+                    fij, vij = compute_pair_interaction(ff, r2)
+                    f[:,i] .+= fij.*rij
+                    f[:,j] .-= fij.*rij
+                    V += vij
+                    W += sum(fij.*rij)
+                end
             end
         end
     end
     ff.potential_energy = V
     ff.virial = 2.0*W
+    return
 end
 
-# Interface that needs to be implemented for each force field
+# Interface for force field implementations
 function compute_pair_interaction(ff::ForceField, r2::Float64)
     error("compute_pair_interaction not implemented for $(typeof(ff))")
 end
